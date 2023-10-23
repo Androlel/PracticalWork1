3413c57d85f54b6a7784115e7a0deb7920e8d610
Androlel <79891684+Androlel@users.noreply.github.com>
2023-10-23 20:53:05 +0200

    4 works but not correctly now

--- image_filter.py
+++ image_filter.py
@@ -1,62 +1,53 @@
 import numpy as np
 
-
-def image_filter(image,
-                 filter_mask,
-                 numprocessors,
-                 filtered_image):
-    #Filter comes in as 2d matrix (filter_mask)
+def image_filter(image, filter_mask, numprocessors, filtered_image):
+    # Filter comes in as a 2D matrix (filter_mask)
     print(filter_mask)
 
-    #Will do this through a for loop with each row
-    (rows,cols,depth) = image.shape
+    # Will do this through a for loop with each row
+    rows, cols, depth = image.shape
 
-    frows = len(filter_mask)    
+    frows = len(filter_mask)
     try:
         fcols = len(filter_mask[0])
-    except:
+    except TypeError:
         # Treat it as a 1D array
         fcols = len(filter_mask)
         frows = 1
-    
-    #Doing integer division to find the midpoint of the filter, this will be how many values we have to offset in each direction
-    midrow = frows//2
-    midcol = fcols//2
+
+    # Doing integer division to find the midpoint of the filter, this will be how many values we have to offset in each direction
+    midrow = frows // 2
+    midcol = fcols // 2
 
     print(frows, fcols)
     print(midrow, midcol)
-    
+
     for r in range(rows):
         for c in range(cols):
-            #Apply filter here
+            # Apply filter here
             filtered_value = 0
 
-            #We start at the midpoint then go forward or backwards depending on the size of the filter
-            #1d array
-            if(midrow == 0):
-                for j in range(-midcol,midcol+1):
+            # We start at the midpoint then go forward or backward depending on the size of the filter
+            # 1D array
+            if midrow == 0:
+                for j in range(-midcol, midcol + 1):
                     if (c + j >= 0 and c + j < cols):
-                        filtered_value += image[r,c+j] * filter_mask[j+midcol]
-            #Some number x 1 array 
-            elif(midcol == 0):
+                        filtered_value += image[r, c + j] * filter_mask[j + midcol]
+            # Some number x 1 array
+            elif midcol == 0:
                 for i in range(-midrow, midrow + 1):
                     if (r + i >= 0 and r + i < rows):
                         # Multiply the filter value with the corresponding pixel value
-                        filtered_value += image[r + i, c] * filter_mask[i+midrow]
-
+                        filtered_value += image[r + i, c] * filter_mask[i + midrow]
             else:
                 for i in range(-midrow, midrow + 1):
                     for j in range(-midcol, midcol + 1):
                         # Check if the current indices are within bounds
                         if (r + i >= 0 and r + i < rows) and (c + j >= 0 and c + j < cols):
                             # Multiply the filter value with the corresponding pixel value
-                            #filtered_value += image[r + i, c + j] * filter_mask[i + midrow][j + midcol]
-                            filtered_value = np.add(filtered_value, image[r + i, c + j] * filter_mask[i + midrow][j + midcol], out= filtered_image[r, c], casting="unsafe")
-
-
+                            filtered_value += np.uint8(image[r + i, c + j] * filter_mask[i + midrow][j + midcol])
 
             # Assign the filtered value to the corresponding pixel in the filtered image
             filtered_image[r, c] = filtered_value
-
 
     return filtered_image
