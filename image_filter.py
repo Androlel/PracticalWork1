35eaa6c9102aa99a76bc3ff2ef3c5f2eac044f2e
Androlel <79891684+Androlel@users.noreply.github.com>
2023-10-23 21:34:10 +0200

    added multiprocessing with locks?

--- image_filter.py
+++ image_filter.py
@@ -1,4 +1,33 @@
 import numpy as np
+import multiprocessing as mp
+
+#Processes each individual row
+def process_row(lock,row, image, filter_mask, midrow, midcol):
+    rows, cols = image.shape[:2]
+    filtered_row = np.zeros((cols, 3), dtype=np.uint8)
+
+    for c in range(cols):
+        filtered_value = 0
+
+        if midrow == 0:
+            for j in range(-midcol, midcol + 1):
+                if (c + j >= 0 and c + j < cols):
+                    filtered_value += image[row, c + j] * filter_mask[j + midcol]
+        elif midcol == 0:
+            for i in range(-midrow, midrow + 1):
+                if (row + i >= 0 and row + i < rows):
+                    filtered_value += image[row + i, c] * filter_mask[i + midrow]
+        else:
+            for i in range(-midrow, midrow + 1):
+                for j in range(-midcol, midcol + 1):
+                    if (row + i >= 0 and row + i < rows) and (c + j >= 0 and c + j < cols):
+                        filtered_value += np.uint8(image[row + i, c + j] * filter_mask[i + midrow][j + midcol])
+
+  
+        with lock:
+            filtered_row[c] = filtered_value
+
+    return row, filtered_row
 
 def image_filter(image, filter_mask, numprocessors, filtered_image):
     # Filter comes in as a 2D matrix (filter_mask)
@@ -22,32 +51,18 @@ def image_filter(image, filter_mask, numprocessors, filtered_image):
     print(frows, fcols)
     print(midrow, midcol)
 
-    for r in range(rows):
-        for c in range(cols):
-            # Apply filter here
-            filtered_value = 0
+    # Create a multiprocessing pool
+    pool = mp.Pool(processes=numprocessors)
+    results = []
+    lock = mp.Manager().Lock()
 
-            # We start at the midpoint then go forward or backward depending on the size of the filter
-            # 1D array
-            if midrow == 0:
-                for j in range(-midcol, midcol + 1):
-                    if (c + j >= 0 and c + j < cols):
-                        filtered_value += image[r, c + j] * filter_mask[j + midcol]
-            # Some number x 1 array
-            elif midcol == 0:
-                for i in range(-midrow, midrow + 1):
-                    if (r + i >= 0 and r + i < rows):
-                        # Multiply the filter value with the corresponding pixel value
-                        filtered_value += image[r + i, c] * filter_mask[i + midrow]
-            else:
-                for i in range(-midrow, midrow + 1):
-                    for j in range(-midcol, midcol + 1):
-                        # Check if the current indices are within bounds
-                        if (r + i >= 0 and r + i < rows) and (c + j >= 0 and c + j < cols):
-                            # Multiply the filter value with the corresponding pixel value
-                            filtered_value += np.uint8(image[r + i, c + j] * filter_mask[i + midrow][j + midcol])
+    # Process rows in parallel
+    results = [pool.apply_async(process_row, args=(lock, r, image, filter_mask, midrow, midcol)) for r in range(rows)]
+    
+    processed_rows = [result.get() for result in results]
 
-            # Assign the filtered value to the corresponding pixel in the filtered image
-            filtered_image[r, c] = filtered_value
+    # Combine the processed rows into the filtered image
+    for r, filtered_row in processed_rows:
+        filtered_image[r, :] = filtered_row
 
     return filtered_image
