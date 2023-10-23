b81a8a0314d3ab74b4619b2b9ef5be0020d96dd3
robglazerumass <rglazer@umass.edu>
2023-10-23 23:44:29 +0200

    Execution.

--- filters_execution.py
+++ filters_execution.py
@@ -1,52 +1,31 @@
 import numpy as np
 import multiprocessing as mp
 import image_filter as im
-from multiprocessing import Process, Queue
 
 
+def apply_filter(image, filter_mask, numprocessors, filtered_image, conn):
+    #applying the filter
+    filtered_image = im.image_filter(image, filter_mask, numprocessors, filtered_image)
+    #We now need to send out the results
+    conn.send_bytes(filtered_image.tobytes())  #To do this we just send the bytes of the numpy array
+    conn.close()
 
-def filters_execution(
-    image,  
-    filter_mask1, 
-    filter_mask2,  
-    numprocessors,
-    filtered_image1, 
-    filtered_image2):
+def filters_execution(image, filter_mask1, filter_mask2,numprocessors, filtered_image1, filtered_image2):
+    #We are going to need to make a pipe for each process so we can get the results back
+    parent_conn1, child_conn1 = mp.Pipe()
+    parent_conn2, child_conn2 = mp.Pipe()
 
-    filtered_image1 = im.image_filter(image,filter_mask1,numprocessors//2,filtered_image1)
-    filtered_image2 = im.image_filter(image,filter_mask2,numprocessors//2,filtered_image2)
-    
-    # # Create two Queues to store the filtered images
-    # queue1 = Queue()
-    # queue2 = Queue()
+    #We are creating and starting two parallel processes to apply the filters
+    process1 = mp.Process(target=apply_filter, args=(image, filter_mask1, numprocessors//2, filtered_image1.copy(), child_conn1))
+    process2 = mp.Process(target=apply_filter, args=(image, filter_mask2, numprocessors//2, filtered_image2.copy(), child_conn2))
+    process1.start()
+    process2.start()
 
-    # # Create two separate processes to filter the image
-    # process1 = Process(target=im.image_filter, args=(image, filter_mask1, numprocessors // 2, queue1))
-    # process2 = Process(target=im.image_filter, args=(image, filter_mask2, numprocessors // 2, queue2))
-
-    # # Start the processes
-    # process1.start()
-    # process2.start()
-
-    # # Wait for both processes to finish
-    # process1.join()
-    # process2.join()
-
-    # # Get the filtered images from the Queues
-    # filtered_image1 = queue1.get()
-    # filtered_image2 = queue2.get()
+    #Getting the results out from the pipe
+    filtered_image1[:] = np.frombuffer(parent_conn1.recv_bytes(), dtype=np.uint8).reshape(filtered_image1.shape)
+    filtered_image2[:] = np.frombuffer(parent_conn2.recv_bytes(), dtype=np.uint8).reshape(filtered_image2.shape)
+    #And done
+    process1.join()
+    process2.join()
 
     return filtered_image1, filtered_image2
-
-
-    # results1 = [pool.apply_async(im.image_filter, args=(image, filter_mask1, numprocessors//2, filtered_image1))]
-    # results2 = [pool.apply_async(im.image_filter, args=(image, filter_mask2, numprocessors//2, filtered_image2))]
-    
-    # Wait for all processes to finish
-    # pool.close()
-    # pool.join()
-
-    # filtered_image1 = result_queue.get()
-    # filtered_image2 = result_queue.get()
-    # filtered_image1 = im.image_filter(image,filter_mask1,numprocessors//2,filtered_image1)
-    # filtered_image2 = im.image_filter(image,filter_mask2,numprocessors//2,filtered_image2)
